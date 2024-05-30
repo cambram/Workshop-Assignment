9f982f4b2cf463545339f29023e7dbc45af905c7
Cameron Brammer <cameroncbrammer@gmail.com>
2024-05-30 13:47:30 +0200

    I/O checks started

--- server.js
+++ server.js
@@ -34,4 +34,48 @@ function division(x, y){
     }
 }
 
+/* Input and Output */
+function isInputLimitReached(input){
+    if(input.toString().length > 3){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isOutputLimitReached(output){
+    if(output.toString().length > 6){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function doesContainNegatives(output){
+    if(output < 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isDecimal(output){
+    if(output % 1 != 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let hexDictionary = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G'];
+function isInvalidInputType(input){
+    //?????
+}
+
+function isInvalidOutput(){
+
+}
+
+/* GUI */
+
 module.exports = {addition, subtraction, multiplication, division};
